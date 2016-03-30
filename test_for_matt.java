7a48cedd0fc5ff2bffec2d7a5fc9fc004e270eec
tsunnyday <collechess123@gmail.com>
2016-03-30 15:31:34 -0500

    test

---
+++ test_for_matt.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+public class test_for_matt{
+	public static void main(String[] argv)
+	{
+		String test_string = "ABCDEF";
+		System.out.print(test_string.toLowerCase() + "\n");
+				
+	}
+}
